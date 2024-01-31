ab052a9b6d6816f49765aa5ebbb4426257bfe9e8
Niroyan Kugaruban <k.niroyan@gmail.com>
2024-01-31 09:39:31 +0530

    logo addes

--- app/Components/Logo.tsx
+++ app/Components/Logo.tsx
@@ -1,23 +1,23 @@
 import Link from "next/link";
 import React from "react";
-
+import Image from "next/image";
 type Props = {};
 
 const Logo = (props: Props) => {
   return (
     <div className="uppercase flex flex-col items-center leading-none justify-center text-center p-5 pt-[2rem]">
       <Link href="/" className="flex items-center justify-center">
-        {/* <Image
-            src="/assets/img/fine - logo.svg"
-            alt="logo img"
-            className="w-2/3 drop-shadow shadow"
-            width={100}
-            height={100}
-          /> */}
-        <div className="flex items-center justify-center flex-col text-white text-center w-full drop-shadow-lg shadow-lg">
+        <Image
+          src="/assets/svg/logo.svg"
+          alt="logo img"
+          className="w-2/3 drop-shadow shadow scale-75"
+          width={100}
+          height={100}
+        />
+        {/* <div className="flex items-center justify-center flex-col text-white text-center w-full drop-shadow-lg shadow-lg">
           <h1 className="text-6xl font-light tracking-[8px] font">fine</h1>
           <span className="text-sm -ml-[6px]">luxury property</span>
-        </div>
+        </div> */}
       </Link>
     </div>
   );
