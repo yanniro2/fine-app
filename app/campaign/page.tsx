44973b419783156a90cb00cabc2ab8b04155be24
Niroyan Kugaruban <k.niroyan@gmail.com>
2024-02-13 23:16:26 +0530

    done ui fixed

--- app/campaign/page.tsx
+++ app/campaign/page.tsx
@@ -6,12 +6,12 @@ type Props = {};
 const page = (props: Props) => {
   return (
     <div className="w-screen h-full flex items-center justify-center pl-[20vw] overflow-auto">
-      <div className="w-full h-full pt-[8rem] p-5 grid grid-cols-3 grid-rows-auto  gap-5">
+      <div className="w-full h-full pt-[8rem] p-5 flex flex-wrap  gap-5 justify-evenly">
         {campaignData.campaigns.map((campaign, index) => (
           <div
             key={index}
-            className="text-white w-full h-full rounded-xl  relative overflow-hidden cursor-pointer  transition-all border-2 hover:border-white hover:scale-105  border-transparent group">
-            <div className="absolute bottom-3 left-1/2 right-0 z-50 -translate-x-1/2 w-3/4 text-center font-Myanmar backdrop-blur-lg p-1 group-hover:backdrop-brightness-50">
+            className="text-white w-[22rem] h-[14rem] rounded-xl  relative overflow-hidden cursor-pointer  transition-all border-2 hover:border-white hover:scale-105  border-transparent group ">
+            <div className="absolute bottom-3 left-1/2 right-0 z-50 -translate-x-1/2 w-3/4 text-center font-Myanmar backdrop-blur-lg p-1 group-hover:backdrop-brightness-50 uppercase">
               {campaign.address}
             </div>
 
@@ -20,8 +20,8 @@ const page = (props: Props) => {
             <Image
               src={campaign.image}
               alt={campaign.name}
-              width={800}
-              height={800}
+              width={1000}
+              height={1000}
               className="w-full h-full overflow-hidden object-cover group-hover:brightness-50 transition-all"
             />
           </div>
